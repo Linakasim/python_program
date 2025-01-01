c402a32550bddf9772d38aa42aaa49058dbb1c01
Linakasim <Linakasim17883@gmail.com>
2025-01-01 15:33:28 +0530

    Add files via upload

---
+++ program14.py
@@ -0,0 +1,4 @@
+color=input("Enter colors seperated by coma:")
+color=list(color.split(','))
+print(color[0])
+print(color[-1])
---
+++ program19.py
@@ -0,0 +1,4 @@
+d1=eval(input("Enter the first dictionary:"))
+d2=eval(input("Enter the second dictionary:"))
+d1.update(d2)
+print(d1)

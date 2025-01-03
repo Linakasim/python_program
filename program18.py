8233f4dc20496ac6851e0bac71d5fe58cd4b116b
Linakasim <Linakasim17883@gmail.com>
2025-01-03 15:44:17 +0530

    Delete program18.py

--- program18.py
+++
@@ -1,6 +0,0 @@
-my_dict={'b':2,'a':1,'d':4,'c':3}
-asc_dict=dict(sorted(my_dict.items()))
-des_dict=dict(sorted(my_dict.items(),reverse=True))
-print("original Dictionary:",my_dict)
-print("Ascending order:",asc_dict)
-print("Descending order:",des_dict)

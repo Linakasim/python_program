0a1d4124efaaa82d855b4b3df84cc4a80e287a5c
Linakasim <Linakasim17883@gmail.com>
2025-01-03 15:44:36 +0530

    Delete program20.py

--- program20.py
+++
@@ -1,8 +0,0 @@
-l=[]
-n=int(input("enter a limit:"))
-for i in range(n):
-    v=int(input("Enter values:"))
-    l.append(v)
-print(l)
-c=[n for x in l if x%2!=0]
-print("list of non even numbers",c)
